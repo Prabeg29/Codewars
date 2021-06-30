e89bb3d3039ca7a5e57cbe76a167fb7a49de1c90
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-06-30 12:59:21 +0545

    Add solution

---
+++ 7kyu/oop#3.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+Task
+
+1. Copy your working solution to the first Kata of this series and paste it here (you may want to complete that first if you haven't already done so).
+2. Rename that class to CurrentUSPresident.
+3. Convert all of the class properties into class constants and all of the class methods into static methods using the syntax taught in the lesson.
+4. You can safely remove the code where an instance of the President class is created.
+*/
+
+// Write your code here :D
+
+class CurrentUSPresident{
+    const name = "Barack Obama";
+
+    public static function greet($name){
+        return 'Hello '. $name.', my name is '.self::name.', nice to meet you!';
+    }
+}
